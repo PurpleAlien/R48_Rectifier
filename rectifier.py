2d96f1745ef225725658d6c5d6253099d8c88d9d
Johan Dams <johan.dams@wrdsystems.co.uk>
2024-12-14 23:01:49 +0200

    Cleanup and refactoring

--- rectifier.py
+++ rectifier.py
@@ -19,152 +19,33 @@ BITRATE = 125000
 READ_COMMANDS = [0x01, 0x02, 0x03, 0x04, 0x05]
 
 # Reads all of the above and a few more at once
-READ_ALL = [0x00, 0xF0, 0x00, 0x80, 0x46, 0xA5, 0x34, 0x00] 
+READ_ALL = [0x00, 0xF0, 0x00, 0x80, 0x46, 0xA5, 0x34, 0x00]
 
 # 62.5A is the nominal current of Emerson/Vertiv R48-3000e and corresponds to 121%
 OUTPUT_CURRENT_RATED_VALUE = 62.5
 OUTPUT_CURRENT_RATED_PERCENTAGE_MIN = 10
 OUTPUT_CURRENT_RATED_PERCENTAGE_MAX = 121
-OUTPUT_CURRENT_RATED_PERCENTAGE = 121
 OUTPUT_VOLTAGE_MIN = 41.0
 OUTPUT_VOLTAGE_MAX = 58.5
-OUTPUT_CURRENT_MIN = 5.5 # 10%, rounded up to nearest 0.5V 
+OUTPUT_CURRENT_MIN = 5.5
 OUTPUT_CURRENT_MAX = OUTPUT_CURRENT_RATED_VALUE
 
-# needs root/sudo access, or configure this part on the OS
+# Helper Functions
+
+# Needs root/sudo access, or configure this part on the OS
 def config(channel):
-    subprocess.call(['ip', 'link', 'set', 'down', channel])
-    subprocess.call(['ip', 'link', 'set', channel, 'type', 'can', 'bitrate', str(BITRATE), 'restart-ms', '1500'])
-    subprocess.call(['ip', 'link', 'set', 'up', channel])
-
-# To convert floating point units to 4 bytes in a bytearray
-def float_to_bytearray(f):
-    value = hex(struct.unpack('<I', struct.pack('<f', f))[0])
-    return bytearray.fromhex(value.lstrip('0x').rstrip('L'))
-
-# Get the bus and send data to the specified CAN bus
-def send_can_message(channel, data):
+    """Configure the CAN interface."""
     try:
-        with can.interface.Bus(bustype='socketcan', channel=channel, bitrate=BITRATE) as bus:
-            msg = can.Message(arbitration_id=ARBITRATION_ID, data=data, is_extended_id=True)
-            bus.send(msg)
-            print(f"Command sent on {bus.channel_info}")
-    except can.CanError:
-        print("Command NOT sent")
+        subprocess.run(['ip', 'link', 'set', 'down', channel], check=True)
+        subprocess.run(['ip', 'link', 'set', channel, 'type', 'can', 'bitrate', str(BITRATE), 'restart-ms', '1500'], check=True)
+        subprocess.run(['ip', 'link', 'set', 'up', channel], check=True)
+        print(f"Configured CAN interface: {channel}")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to configure CAN interface {channel}: {e}")
 
-# CAN message receiver
-def receive_can_message(channel):
-    try:
-        with can.interface.Bus(receive_own_messages=True, bustype='socketcan', channel=channel, bitrate=BITRATE) as bus:
-          #print_listener = can.Printer()
-          #can.Notifier(bus, [print_listener])
-          can.Notifier(bus, [can_listener])
-          
-          # Keep sending requests for all data every second 
-          while True:
-            # Individually
-            #for p in READ_COMMANDS: 
-            #    data = [0x01, 0xF0, 0x00, p, 0x00, 0x00, 0x00, 0x00]
-            #    msg = can.Message(arbitration_id=ARBITRATION_ID_READ, data=data, is_extended_id=True)
-            #    bus.send(msg) 
-            #    time.sleep(0.1)
-
-            # All at once
-            msg = can.Message(arbitration_id=ARBITRATION_ID_READ, data=READ_ALL, is_extended_id=True)
-            bus.send(msg)
-            time.sleep(1.0)
-    except can.CanError:
-        print("Receive went wrong")
-
-# CAN receiver listener
-def can_listener(msg):
-    # Is it a response to our request
-    if msg.data[0] == 0x41:
-        # Convert value to float (it's the same for all)
-        val = struct.unpack('>f', msg.data[4:8])[0]
-        # Check what data it is
-        match msg.data[3] :
-            case 0x01:
-                print("Vout (VDC) : " + str(val))
-            case 0x02:
-                print("Iout (IDC) : " + str(val))
-            case 0x03:
-                print("Output Current Limit : " + str(val))
-            case 0x04:
-                print("Temp (C) : " + str(val))
-            case 0x05:
-               print("Vin (VAC) : " + str(val)) 
-
-# Set the output voltage to the new value. 
-# The 'fixed' parameter 
-#  - if True makes the change permanent ('offline command')
-#  - if False the change is temporary (30 seconds per command received, 'online command', repeat at 15 second intervals).
-# Voltage between 41.0 and 58.5V - fan will go high below 48V!
-def set_voltage(channel, voltage, fixed=False):
-    if OUTPUT_VOLTAGE_MIN <= voltage <= OUTPUT_VOLTAGE_MAX:
-        b = float_to_bytearray(voltage)
-        p = 0x21 if not fixed else 0x24
-        data = [0x03, 0xF0, 0x00, p, *b]
-        send_can_message(channel, data)
-    else:
-        print(f"Voltage should be between {OUTPUT_VOLTAGE_MIN}V and {OUTPUT_VOLTAGE_MAX}V")
-
-# The output current is set in percent to the rated value of the rectifier written in the manual
-# Possible values for 'current': 10% - 121% (rated current in the datasheet = 121%)
-# The 'fixed' parameter
-#  - if True makes the change permanent ('offline command')
-#  - if False the change is temporary (30 seconds per command received, 'online command', repeat at 15 second intervals).
-def set_current_percentage(channel, current, fixed=False):
-    if OUTPUT_CURRENT_RATED_PERCENTAGE_MIN <= current <= OUTPUT_CURRENT_RATED_PERCENTAGE_MAX:
-        limit = current / 100
-        b = float_to_bytearray(limit)
-        p = 0x22 if not fixed else 0x19
-        data = [0x03, 0xF0, 0x00, p, *b]
-        send_can_message(channel, data)
-    else:
-        print(f"Current should be between {OUTPUT_CURRENT_RATED_PERCENTAGE_MIN}% and {OUTPUT_CURRENT_RATED_PERCENTAGE_MAX}%")
-
-# The output current is set as a value
-# Possible values for 'current': 5.5A - 62.5A
-# The 'fixed' parameter
-#  - if True makes the change permanent ('offline command')
-#  - if False the change is temporary (30 seconds per command received, 'online command', repeat at 15 second intervals).
-def set_current_value(channel, current, fixed=False): 
-    if OUTPUT_CURRENT_MIN <= current <= OUTPUT_CURRENT_MAX:
-        # 62.5A is the nominal current of Emerson/Vertiv R48-3000e and corresponds to 121%
-        percentage = (current/OUTPUT_CURRENT_RATED_VALUE)*OUTPUT_CURRENT_RATED_PERCENTAGE
-        set_current_percentage(channel , percentage, fixed)
-    else:
-        print(f"Current should be between {OUTPUT_CURRENT_MIN}A and {OUTPUT_CURRENT_MAX}A")
-
-
-
-# Time to ramp up the rectifiers output voltage to the set voltage value, and enable/disable
-def walk_in(channel, time=0, enable=False):
-    if not enable:
-        data = [0x03, 0xF0, 0x00, 0x32, 0x00, 0x00, 0x00, 0x00]
-    else:
-        data = [0x03, 0xF0, 0x00, 0x32, 0x00, 0x01, 0x00, 0x00]
-        b = float_to_bytearray(time)
-        data.extend(b)
-    send_can_message(channel, data)
-
-# AC input current limit (called Diesel power limit): gives the possibility to reduce the overall power of the rectifier
-def limit_input(channel, current):
-    b = float_to_bytearray(current)
-    data = [0x03, 0xF0, 0x00, 0x1A, *b]
-    send_can_message(channel, data)
-
-# Restart after overvoltage enable/disable
-def restart_overvoltage(channel, state=False):
-    if not state:
-        data = [0x03, 0xF0, 0x00, 0x39, 0x00, 0x00, 0x00, 0x00]
-    else:
-        data = [0x03, 0xF0, 0x00, 0x39, 0x00, 0x01, 0x00, 0x00]
-    send_can_message(channel, data)
-
-# Function to parse boolean values (case-insensitive)
+# Converts a few possible strings to boolean
 def str_to_bool(value):
+    """Parse string to boolean (case-insensitive)."""
     if isinstance(value, bool):
         return value
     if value.lower() in {'true', 't', 'yes', '1'}:
@@ -174,91 +55,153 @@ def str_to_bool(value):
     else:
         raise argparse.ArgumentTypeError(f"Invalid boolean value: {value}")
 
+# To convert floating point units to 4 bytes in a bytearray
+def float_to_bytearray(f):
+    """Convert a float to a 4-byte array."""
+    return bytearray(struct.pack('<f', f))
 
+# Check if options passed are within accepted range
+def validate_range(value, min_val, max_val, name):
+    """Validate that a value is within the specified range."""
+    if not (min_val <= value <= max_val):
+        raise ValueError(f"{name} must be between {min_val} and {max_val}. Got {value}.")
+    return value
+
+# Get the bus and send data to the specified CAN bus
+def send_can_message(channel, data):
+    """Send a message to the CAN bus."""
+    try:
+        with can.interface.Bus(bustype='socketcan', channel=channel, bitrate=BITRATE) as bus:
+            msg = can.Message(arbitration_id=ARBITRATION_ID, data=data, is_extended_id=True)
+            bus.send(msg)
+            print(f"Command sent on {bus.channel_info}")
+    except can.CanError as e:
+        print(f"CAN message send error: {e}")
+
+# CAN message receiver
+def receive_can_message(channel):
+    """Receive and print messages from the CAN bus."""
+    try:
+        with can.interface.Bus(receive_own_messages=True, bustype='socketcan', channel=channel, bitrate=BITRATE) as bus:
+            #print_listener = can.Printer()
+            #can.Notifier(bus, [print_listener])
+            can.Notifier(bus, [can_listener])
+
+            # Keep sending requests for all data every second
+            while True:
+                # Individually
+                #for p in READ_COMMANDS:
+                #    data = [0x01, 0xF0, 0x00, p, 0x00, 0x00, 0x00, 0x00]
+                #    msg = can.Message(arbitration_id=ARBITRATION_ID_READ, data=data, is_extended_id=True)
+                #    bus.send(msg)
+                #    time.sleep(0.1)
+                
+                # All at once
+                msg = can.Message(arbitration_id=ARBITRATION_ID_READ, data=READ_ALL, is_extended_id=True)
+                bus.send(msg)
+                time.sleep(1.0)
+    except can.CanError as e:
+        print(f"CAN message receive error: {e}")
+
+# CAN receiver listener
+def can_listener(msg):
+    """Process received CAN messages."""
+    # Is it a response to our request
+    if msg.data[0] == 0x41:
+        # Convert value to float (it's the same for all)
+        val = struct.unpack('>f', msg.data[4:8])[0]
+        # Check what data it is
+        match msg.data[3]:
+            case 0x01: print(f"Vout (VDC): {val}")
+            case 0x02: print(f"Iout (IDC): {val}")
+            case 0x03: print(f"Output Current Limit: {val}")
+            case 0x04: print(f"Temp (°C): {val}")
+            case 0x05: print(f"Vin (VAC): {val}")
+
+# Set the output voltage to the new value. 
+# The 'fixed' parameter 
+#  - if True makes the change permanent ('offline command')
+#  - if False the change is temporary (30 seconds per command received, 'online command', repeat at 15 second intervals).
+# Voltage between 41.0 and 58.5V - fan will go high below 48V!
+def set_voltage(channel, voltage, fixed=False):
+    """Set output voltage."""
+    validate_range(voltage, OUTPUT_VOLTAGE_MIN, OUTPUT_VOLTAGE_MAX, "Voltage")
+    cmd = 0x24 if fixed else 0x21
+    send_can_message(channel, [0x03, 0xF0, 0x00, cmd, *float_to_bytearray(voltage)])
+
+# The output current is set in percent to the rated value of the rectifier written in the manual
+# Possible values for 'current': 10% - 121% (rated current in the datasheet = 121%)
+# The 'fixed' parameter
+#  - if True makes the change permanent ('offline command')
+#  - if False the change is temporary (30 seconds per command received, 'online command', repeat at 15 second intervals)
+def set_current_percentage(channel, percent, fixed=False):
+    """Set output current as a percentage."""
+    validate_range(percent, OUTPUT_CURRENT_RATED_PERCENTAGE_MIN, OUTPUT_CURRENT_RATED_PERCENTAGE_MAX, "Current percentage")
+    cmd = 0x19 if fixed else 0x22
+    send_can_message(channel, [0x03, 0xF0, 0x00, cmd, *float_to_bytearray(percent / 100)])
+
+# The output current is set as a value
+# Possible values for 'current': 5.5A - 62.5A
+# The 'fixed' parameter
+#  - if True makes the change permanent ('offline command')
+#  - if False the change is temporary (30 seconds per command received, 'online command', repeat at 15 second intervals).
+def set_current_value(channel, current, fixed=False):
+    """Set output current in amps."""
+    validate_range(current, OUTPUT_CURRENT_MIN, OUTPUT_CURRENT_MAX, "Current")
+    percent = (current / OUTPUT_CURRENT_RATED_VALUE) * OUTPUT_CURRENT_RATED_PERCENTAGE_MAX
+    set_current_percentage(channel, percent, fixed)
+
+# Time to ramp up the rectifiers output voltage to the set voltage value, and enable/disable
+def walk_in(channel, time=0, enable=False):
+    """Enable or disable walk-in functionality."""
+    data = [0x03, 0xF0, 0x00, 0x32, 0x00, 0x01 if enable else 0x00, 0x00, 0x00]
+    if enable:
+        data.extend(float_to_bytearray(time))
+    send_can_message(channel, data)
+
+# AC input current limit (called Diesel power limit): gives the possibility to reduce the overall power of the rectifier
+def limit_input(channel, current):
+    """Set AC input current limit."""
+    send_can_message(channel, [0x03, 0xF0, 0x00, 0x1A, *float_to_bytearray(current)])
+
+# Restart after overvoltage enable/disable
+def restart_overvoltage(channel, state=False):
+    """Enable or disable restart after overvoltage."""
+    send_can_message(channel, [0x03, 0xF0, 0x00, 0x39, 0x00, 0x01 if state else 0x00, 0x00, 0x00])
+
+# Main
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Set/Get Parameters from Emerson/Vertiv Rectifiers.')
+    parser = argparse.ArgumentParser(description="Control Emerson/Vertiv Rectifiers via CAN.")
+    parser.add_argument("-m", "--mode", choices=["set", "get"], help="Operation mode: set or get")
+    parser.add_argument("-v", "--voltage", type=float, help="Set output voltage (41.0V - 58.5V)")
+    parser.add_argument("-cv", "--current_value", type=float, help="Set output current in amps (5.5A - 62.5A)")
+    parser.add_argument("-cp", "--current_percent", type=float, help="Set output current as percentage (10%% - 121%%)")
+    parser.add_argument("-p", "--permanent", action="store_true", help="Make settings permanent")
+    parser.add_argument("-I", "--interface", default="can0", help="CAN interface (default: can0)")
+    parser.add_argument("-C", "--configure", action="store_true", help="Configure CAN interface")
+    parser.add_argument("--walk_in", type=str_to_bool, help="Enable or disable walk-in")
+    parser.add_argument("--walk_in_time", type=float, help="Walk-in ramp-up time (seconds)")
+    parser.add_argument("--limit_input", type=float, help="Set AC input current limit (amps)")
+    parser.add_argument("--restart_overvoltage", type=str_to_bool, help="Enable or disable restart after overvoltage")
 
-    parser.add_argument('-m', '--mode', default="none",
-                    help='Mode of Operation (set/get)')
+    args = parser.parse_args()
 
-    # DC voltage
-    parser.add_argument('-v', '--voltage', type=float,
-                    help='Voltage Set Point of the Charger (41.0VDC - 58.5VDV)')
-
-    # DC current
-    parser.add_argument('-cv', '--current_value', type=float,
-                    help='Current Set Point of the Charger (5.5ADC - 62.5ADC)')
-    parser.add_argument('-cp', '--current_percent', type=float,
-                    help='Current Set Point of the Charger in percent (10%% - 121%%)')
-
-    # Permanent settings for the above
-    parser.add_argument('-p', '--permanent', action='store_true',
-                    help='Make settings permanent')
-
-    # CAN interfaace configuration
-    parser.add_argument('-I', '--interface', default="can0",
-                    help='Adapter Interface (can0, can1, ...)')
-
-    parser.add_argument('-C', '--configure', action='store_true',
-                    help='Configure link (bitrate, bring up interface) as well') 
-
-    # Walk in (ramp up DC voltage over time)
-    parser.add_argument('--walk_in', type=bool, 
-                        help='Enable (True) or disable (False) the walk-in process.')
-    parser.add_argument('--walk_in_time', type=float, 
-                        help='Time for walk-in ramp-up process in seconds.')
-
-    # AC input limiter (Diesel power limit). Doesn't seem to have a permanent option
-    parser.add_argument('--limit_input', type=float,
-                        help='Set AC input current limit (Diesel power limit) in Amps.')
-
-    # Do we restart automatically after overvoltage?
-    parser.add_argument('--restart_overvoltage', type=bool,
-                        help='Enable (True) or disable (False) restart after overvoltage.')
-
-
-    args = parser.parse_args()    
-
-    if args.configure == True:
-        config(args.interface)    
+    if args.configure:
+        config(args.interface)
 
     if args.mode == "set":
-        # DC voltage
-        if args.voltage is not None:
+        if args.voltage:
             set_voltage(args.interface, args.voltage, args.permanent)
-
-        # DC current
-        if args.current_value is not None:
+        if args.current_value:
             set_current_value(args.interface, args.current_value, args.permanent)
-
-        if args.current_percent is not None:
+        if args.current_percent:
             set_current_percentage(args.interface, args.current_percent, args.permanent)
-
-        # Walk in
         if args.walk_in is not None:
-            time = args.walk_in_time if args.walk_in_time is not None else 0.0
-            walk_in(args.interface, time=time, enable=args.walk_in)
-        elif args.walk_in_time is not None:
-            # If time is set but walk_in is not provided, raise an error
-            print("Error: '--walk_in_time' provided without '--walk_in'. Please specify '--walk_in True' or '--walk_in False'.")
-            exit(1)
-
-        # Input limit
-        if args.limit_input is not None:
+            walk_in(args.interface, time=args.walk_in_time or 0, enable=args.walk_in)
+        if args.limit_input:
             limit_input(args.interface, args.limit_input)
-
-        # Restart after overvoltage
         if args.restart_overvoltage is not None:
             restart_overvoltage(args.interface, state=args.restart_overvoltage)
-
-    elif args.mode== "get":
+    elif args.mode == "get":
         receive_can_message(args.interface)
 
-
-
-    #config('can0')
-    #set_voltage('can0', 52.0, False)
-    #set_current('can0', 10.0, False)
-    #walk_in('can0', False)
-    #limit_input('can0', 10.0)
-    #restart_overvoltage('can0', False)
